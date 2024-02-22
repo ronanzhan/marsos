65584939342d35e3ffc028667d103b1701588276
ronanzhan <ronan.zhan@gmail.com>
2024-02-22 20:07:05 +0800

    run in qemu

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 #![no_main]
 
+static HELLO: &[u8] = b"Hello World!";
+
 
 use core::panic::PanicInfo;
 
@@ -12,5 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
